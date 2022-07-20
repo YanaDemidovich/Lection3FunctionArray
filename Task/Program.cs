f6cc8e1a7ce5b2bab2ae2f4a0922059cc5a12b9f
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-20 23:01:31 +0300

    Save 3

--- Task/Program.cs
+++ Task/Program.cs
@@ -103,3 +103,40 @@ for(int i = 2; i <= 10; i++)
     Console.WriteLine();
 } 
 */
+
+// ====== Работа с текстом
+// Дан текст. В тексте нужно заменить пробелы черточками, 
+// маленькие "к" на большие "К", 
+// и большие "С", заменить на маленькие "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаяCь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие пруCCкого короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012 
+// s[3]  // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '/');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+newText = Replace(newText, 'C', 'c');
+Console.WriteLine(newText);
+
