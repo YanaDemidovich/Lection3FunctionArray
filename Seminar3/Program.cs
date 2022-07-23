1d8dd817442363b930f5c0b6fb350c06cbdc88fe
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-23 23:11:39 +0300

    Save Task 1

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,5 +1,5 @@
 ﻿// ====== К какой четверти относится заданная координата? ======
-int FindQuart(int x, int y)
+/* int FindQuart(int x, int y)
 {
     if(x > 0 && y > 0) return 1;
     if(x < 0 && y > 0) return 2;
@@ -13,4 +13,42 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input Y coord: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Number of quart for out point is " + FindQuart(x,y));
+int quart = FindQuart(x,y);
+if (quart == -1)
+    Console.WriteLine("Point on the axes!");
+else
+    Console.WriteLine("Number of quart for out point is " + quart);
+*/
+
+// Задача1. Вводим номер четверти координат, на вывод определяем диапазон чисел. 
+// Например: вводим №1, на ввод "х больше 0 и y больше 0"
+
+/* void QuarterNumber(int quart)
+{
+    if(quart == 1) Console.WriteLine("x больше 0, y больше 0");
+    if(quart == 2) Console.WriteLine("x меньше 0, y больше 0");
+    if(quart == 3) Console.WriteLine("x больше 0, y меньше 0");
+    if(quart == 4) Console.WriteLine("x меньше 0, y меньше 0");
+    if(quart > 4 || quart < 1) Console.WriteLine("Такой четверти не существует");
+}
+Console.Write("Input quarter: ");
+int quart = Convert.ToInt32(Console.ReadLine());
+
+QuarterNumber(quart);*/
+// === или ===
+/*void QuarterNumber(int x)
+{
+    if(x == 1) Console.WriteLine("x больше 0, y больше 0");
+    if(x == 2) Console.WriteLine("x меньше 0, y больше 0");
+    if(x == 3) Console.WriteLine("x больше 0, y меньше 0");
+    if(x == 4) Console.WriteLine("x меньше 0, y меньше 0");
+    if(x > 4 || x < 1) Console.WriteLine("Такой четверти не существует");
+}
+
+Console.Write("Input quarter: ");
+int x = Convert.ToInt32(Console.ReadLine());
+QuarterNumber( x);*/
+
+
+// Задача 2. Принимаем число n, а на вывод квадраты чисел от 1 до введенного числа n.
+// Задача 3. Найти расстояние между двумя точками А и В в плоскости.
