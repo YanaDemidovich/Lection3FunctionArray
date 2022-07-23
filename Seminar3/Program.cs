752d2b5ba0f18874a68777c743e3ee8ad234c9ad
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-23 22:04:19 +0300

    Save Task 1

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ====== К какой четвери относится заданная координата? ======
+int FindQuart(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x > 0 && y < 0) return 3;
+    if(x < 0 && y < 0) return 4;
+    return -1;
+}
+
+Console.Write("Input X coord: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Y coord: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number of quart for out point is " + FindQuart(x,y));
