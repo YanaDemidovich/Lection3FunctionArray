6c5c4882657106654e9068dc5e1084492d2afab5
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-24 00:53:01 +0300

    Save Homework Task2

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -2,9 +2,7 @@
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 
-
-
-int PrintArray( int n1, int n2, int n3, int n4, int n5)
+/*int PrintArray( int n1, int n2, int n3, int n4, int n5)
 {
     if(n1 == n5 && n2 == n4) return 1;
     return -1;
@@ -24,4 +22,31 @@ int num = PrintArray(n1, n2, n3, n4, n5);
 if (num == -1)
     Console.WriteLine(n1+""+n2+""+n3+""+n4+""+n5+""+" " + "НЕ является палиндромом ");
 else
-    Console.WriteLine(n1+""+n2+""+n3+""+n4+""+n5+""+" " + "является палиндромом ");
+    Console.WriteLine(n1+""+n2+""+n3+""+n4+""+n5+""+" " + "является палиндромом ");*/
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
+
+/* double Pifagor(double x1, double y1, double x2, double y2, double x3, double y3)
+
+{    
+    double gipotenuza = Math.Sqrt((x1-x2)*(x1-x2)* + (y1-y2)*(y1-y2) + (x2-x3)*(x2-x3)* + (y2-y3)*(y2-y3));
+    return gipotenuza;
+}
+
+Console.Write("Input x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input x3: ");
+double x3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y3: ");
+double y3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ansver is " + Pifagor(x1, y1, x2, y2, x3, y3));*/
+
