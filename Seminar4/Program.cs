1567c27311317d57296586ece8ebf40a133a3b44
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-24 14:03:39 +0300

    save task 2

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -36,4 +36,21 @@ Console.WriteLine("Количество цифр " + Kol(a));*/
 
 
 // Задача 2: Принимает число "n" и выдаст произведение чисел от 1 до "n" (факториал)
+
+/* int Factorial(int a)
+{
+    int current = 1, f = 1;
+    while(current <= a)
+    {
+        f = f * current;
+        current++;
+    }
+    return f;    
+}
+
+Console.Write("Введите целое положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Факториал = " + Factorial(n));*/
+
 // Задача 3: Принимает 2 числа "a" и "b" и возводит число "a" в натуральную степень "b".
