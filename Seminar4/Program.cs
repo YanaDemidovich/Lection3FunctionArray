1d0488787d19b2f09ad7279f427c156d8dbb34e0
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-24 13:48:21 +0300

    save task 1

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход число "a" и возвращает сумму чисел от 1 до "a".
+
+/* int FindSum(int a)
+{
+    int current = 1, sum = 0;
+    while(current <= a)
+    {
+        sum+= current;
+        current++;
+    }
+    return sum;    
+}
+
+Console.Write("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма чисел равна " + FindSum(num));*/
+
+// Задача 1: Принимает число "a"и возвращает кол-во цифр в числе.
+
+/*int Kol(int a)
+{
+    int b = 0;
+    while(a > 0)
+    {
+        b++;
+        a = a/10;
+    }
+    return b;
+}
+
+Console.Write("Введите целое положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Количество цифр " + Kol(a));*/
+
+
+// Задача 2: Принимает число "n" и выдаст произведение чисел от 1 до "n" (факториал)
+// Задача 3: Принимает 2 числа "a" и "b" и возводит число "a" в натуральную степень "b".
