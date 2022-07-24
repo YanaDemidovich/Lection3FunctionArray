7682de2affe77a6e57911289311c659437265f89
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-24 14:51:34 +0300

    save task 27 (нужно исправить, неверно высчитывает)

---
+++ Homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int a)
+{
+    int current = 0, sum = 0;
+    while(current <= a)
+    {
+        sum = sum + a % 10;
+        a = a/10;
+        current++;
+    }
+    return sum;    
+}
+
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр в числе равна " + Sum(n1));
