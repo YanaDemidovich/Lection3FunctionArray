4fcd451b0b5dfc79dee82f29305636ef5198dd1a
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-24 22:30:48 +0300

    save task 27 (что-то не выходирт)

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -3,16 +3,19 @@
 int Sum(int a)
 {
     int current = 0, sum = 0;
-    while(current <= a)
+    while(current < a)
     {
-        sum = sum + a % 10;
+        sum = a % 10;
         a = a/10;
         current++;
     }
-    return sum;    
+    return sum;
 }
 
 Console.Write("Введите число: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Сумма цифр в числе равна " + Sum(n1));
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
