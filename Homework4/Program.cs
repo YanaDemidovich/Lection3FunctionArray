cb2aee4afef37cb8f88cfbabc2a36def05db53d1
YanaDemidovich <108095617+YanaDemidovich@users.noreply.github.com>
2022-07-30 14:58:22 +0300

    Update Program.cs

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-/*int Sum(int a)
+int Sum(int a)
 {
     int result = 0;
     while(a > 0)
@@ -13,13 +13,38 @@
 
 Console.Write("Введите число: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма цифр в числе равна " + Sum(n1));
 
-Console.WriteLine("Сумма цифр в числе равна " + Sum(n1));*/
+// другой вариант решения задачи
+
+int Summa( int number)
+{
+int sum = 0;
+int N = 0;
+int number1 = number;
+while (number1>0)
+{
+    number1 = number1 / 10;
+    N++;
+}
+while (number>0)
+{
+    int a = 0;
+    a = number % 10;
+    number = number / 10;
+    sum = sum + a;
+}
+    return sum;
+}
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = Summa(a);
+Console.WriteLine("Сумма цифр числа = " + result);
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-/* void Massiv()
+void Massiv()
 {
     
     int[] array = new int[8];
@@ -31,4 +56,20 @@ Console.WriteLine("Сумма цифр в числе равна " + Sum(n1));*/
         Console.Write("{0} ", array[i]);
     }
 }
-Massiv(); */
+Massiv();
+
+// другой вариант решения задачи
+
+void Massiv()
+{
+    int N = 8;
+    int[] array = new int[N];
+    Random rand = new Random();
+    Console.WriteLine("Массив чисел: ");
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = rand.Next();
+        Console.Write(" " + array[i]);
+    }
+} 
+Massiv();
